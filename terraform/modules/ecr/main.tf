1ea9568166730bbcd9e3bc87d7d5f0e230580f25
abhiraghuram02 <raghuraman.ab@northeastern.edu>
2026-03-25 19:39:54 -0400

    ECR delete fix

--- terraform/modules/ecr/main.tf
+++ terraform/modules/ecr/main.tf
@@ -1,6 +1,7 @@
 resource "aws_ecr_repository" "main" {
   name                 = var.repository_name
   image_tag_mutability = "MUTABLE"
+  force_delete         = true
 
   image_scanning_configuration {
     scan_on_push = false
@@ -9,7 +10,6 @@ resource "aws_ecr_repository" "main" {
   tags = { Name = var.repository_name }
 }
 
-# Keep only the last 3 images to avoid storage costs on student account
 resource "aws_ecr_lifecycle_policy" "main" {
   repository = aws_ecr_repository.main.name
 
